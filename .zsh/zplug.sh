77deac009642f4a2ed44fe351760cdd8db89caa0
Akinori Tomita <tommy@328w.co.jp>
2017-10-04 11:36:06 +0900

    zsh-autosuggestions インストール

--- .zsh/zplug.sh
+++ .zsh/zplug.sh
@@ -6,6 +6,7 @@ if [ -e ~/.zplug/init.zsh ] ; then
 
     zplug "zsh-users/zsh-syntax-highlighting", defer:2
     zplug "zsh-users/zsh-completions"
+    zplug "zsh-users/zsh-autosuggestions"
     zplug "docker/cli", as:command, use:contrib/completion/zsh/_docker
     zplug "docker/compose", as:command, use:contrib/completion/zsh/_docker-compose
 

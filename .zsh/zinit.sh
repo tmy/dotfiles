664ea9aa0796bfa63cb197f2f3a9c0b06a9fb21a
Akinori Tomita <akinori.tomita@camp-fire.jp>
2024-03-14 12:03:57 +0900

    zinit の環境が壊れていたので修正
    https://qiita.com/taiyodayo/items/c1ebdc863e6baa18ea06

--- .zsh/zinit.sh
+++ .zsh/zinit.sh
@@ -1,25 +1,24 @@
 # -*- zsh -*-
 
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
 function install-zinit() {
-  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
-  command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-  command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-    print -P "%F{160}▓▒░ The clone has failed.%f%b"
+  [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+  [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 }
 
-if [ -e "$HOME/.zinit/bin/zinit.zsh" ] ; then
-  source "$HOME/.zinit/bin/zinit.zsh"
+if [ -e "${ZINIT_HOME}/zinit.zsh" ] ; then
+  source "${ZINIT_HOME}/zinit.zsh"
   autoload -Uz _zinit
   (( ${+_comps} )) && _comps[zinit]=_zinit
 
   # Load a few important annexes, without Turbo
   # (this is currently required for annexes)
   zinit light-mode for \
-      zinit-zsh/z-a-rust \
-      zinit-zsh/z-a-as-monitor \
-      zinit-zsh/z-a-patch-dl \
-      zinit-zsh/z-a-bin-gem-node \
+      zdharma-continuum/z-a-rust \
+      zdharma-continuum/z-a-as-monitor \
+      zdharma-continuum/z-a-patch-dl \
+      zdharma-continuum/z-a-bin-gem-node \
       zsh-users/zsh-autosuggestions \
       zsh-users/zsh-syntax-highlighting \
       zsh-users/zsh-completions
